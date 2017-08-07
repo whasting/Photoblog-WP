d23c43b0f5926369f56d11976a66b06cae1779df
whasting <hastingswade1@gmail.com>
2017-08-07 10:58:04 -0700

    add hero iamge and gallery title shortcodes

--- functions.php
+++ functions.php
@@ -1,5 +1,26 @@
 <?php
+  function add_hero_image( $atts ) {
+    if ( has_post_thumbnail() ) {
+
+      echo "<div class='hero'>" .
+              "<div class='hero-img' style='background-image: url(" .
+              get_the_post_thumbnail_url() .
+              ")'></div>" .
+            "</div>";
+
+    }
+  }
+
+  function add_gallery_title( $atts ) {
+    echo "<div class='gallery-title'>" .
+         "<div class='line'></div>" .
+         "<h1 class='gallery-title-text'>" . $atts["title"] . "</h1>" .
+         "<div class='line'></div>" .
+         "</div>";
+  }
+
   add_theme_support( 'post-thumbnails' );
   add_filter( 'use_default_gallery_style', '__return_false' );
   add_shortcode('hero-img', 'add_hero_image');
+  add_shortcode('gallery-title', 'add_gallery_title');
 ?>
